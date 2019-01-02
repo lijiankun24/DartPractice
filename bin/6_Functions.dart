0ea7131e18f35eaa1255bb05d7966f1d853f4144
lijiankun24 <jiankunli24@gmail.com>
2019-01-02 21:40:38 +0800

    add some code about Function

--- bin/6_Functions.dart
+++ bin/6_Functions.dart
@@ -1,3 +1,8 @@
+/**
+ * 每一个应用都有一个默认的顶级入口函数 main(),
+ * main() 函数有一个可选的 List<String> 类型的参数
+ */
+bool topLevel = true;
 main() {
   // Dart 语言是一门面向对象的语言，即使是函数也可以看做是一个对象，
   // 也就是说函数可以赋值给一个变量，也可以当做参数传入另一个函数中
@@ -8,6 +13,38 @@ main() {
   print(isOS);
 
   print(intDouble(20));
+
+  testNamedFunction('xiaoming');
+//  testNamedFunction('xiaohua', age: 24, gender: '女');
+//  testNamedFunction('xiaogang', gender: '不明');
+
+  testNamedFunction1('iphone7', 'hello dart!');
+  testNamedFunction1('iphone7', 'hello dart!', 'apple');
+//   testNamedFunction1('huawei', 'hello dart!', device: 'android');
+
+  testNamedFunction2('xiaoming', 'hello dart');
+  testNamedFunction2('xiaoming', 'hello dart', 'weixin');
+  testNamedFunction2('xiaoming', 'hello dart', 'weixin', 'in happiness');
+
+  doStaff();
+
+  functionAsObject();
+  anonymousFunction();
+
+  var insideMain = true;
+
+  void myFunction() {
+    var insideFunction = true;
+
+    void nestedFunction() {
+      var insideNestedFunction = true;
+
+      assert(topLevel);
+      assert(insideMain);
+      assert(insideFunction);
+      assert(insideNestedFunction);
+    }
+  }
 }
 
 bool isPhoneFunction(var phoneName) {
@@ -27,12 +64,87 @@ intDouble(int num) => num * 2;
 /**
  * 函数包含可选的参数
  *
- * testNamedFunction('xiaoming');
- * testNamedFunction('xiaohua', age: 24, gender: '女');
- * testNamedFunction('xiaogang', gender: '不明');
+ * testNamedFunction('xiaoming');                       // The name is xiaoming, the age is null, the gender is default
+ * testNamedFunction('xiaohua', age: 24, gender: '女'); // The name is xiaohua, the age is 24, the gender is 女
+ * testNamedFunction('xiaogang', gender: '不明');       // The name is xiaogang, the age is null, the gender is 不明
  */
-void testNamedFunction(String name, {int age = 20, String gender = '男'}) {
-  print('The name is $name');
-  print('The age is $age');
-  print('The gender is $gender');
+void testNamedFunction(String name,
+    {int age = null, String gender = 'default'}) {
+  print('The name is $name, the age is $age, the gender is $gender');
+}
+
+/**
+ * 可选参数也可以使用[]包围，如果使用[]包围，则可选参数必须放到最后
+ *
+ * testNamedFunction1('iphone7', 'hello dart!');                    // The from is iphone7, the msg is hello dart!, the device is default
+ * testNamedFunction1('iphone7', 'hello dart!', 'apple');           // The from is iphone7, the msg is hello dart!, the device is apple
+ * testNamedFunction1('huawei', 'hello dart!', device: 'android');  // error，Found this candidate, but the arguments don't match.
+ */
+void testNamedFunction1(String from, String msg, [String device = 'default']) {
+  print('The from is $from, the msg is $msg, the device is $device');
+}
+
+/**
+ * testNamedFunction2('xiaoming', 'hello dart')                               // xiaoming say hello dart in default
+ * testNamedFunction2('xiaoming', 'hello dart', 'weixin');                    // xiaoming say hello dart in weixin
+ * testNamedFunction2('xiaoming', 'hello dart', 'weixin', 'in happiness');    // xiaoming say hello dart in weixin(in happiness)
+ */
+void testNamedFunction2(String from, String msg,
+    [String channel = 'default', String mode]) {
+  var result = '$from say $msg';
+  if (channel != null) {
+    result = '$result in $channel';
+  }
+  if (mode != null) {
+    result = '$result($mode)';
+  }
+  print(result);
+}
+
+/**
+ * 可选参数的默认值必须是编译时的常量
+ *
+ * [xiaoming, xiaohua, xiaoyang]
+ * {one: apple, two: socks, three: computer}
+ */
+doStaff(
+    {List worker = const ['xiaoming', 'xiaohua', 'xiaoyang'],
+    Map gifts = const {'one': 'apple', 'two': 'socks', 'three': 'computer'}}) {
+  print(worker);
+  print(gifts);
+}
+
+/**
+ * 函数可以作为一个对象，可以用来作为一个函数的参数，也可以赋值给一个变量
+ */
+functionAsObject() {
+  var list = ['one', 'two', 'three', 'four', 'five'];
+  list.forEach(printElement);
+
+  var function = (var msg) {
+    print(msg);
+  };
+  function('hello from xiaoming');
+
+  var louder = (String msg) {
+    print("!!!${msg.toUpperCase()}!!!");
+  };
+  louder('hello');
+}
+
+printElement(var element) {
+  print(element);
+}
+
+/**
+ * 匿名函数，形如下，参数类型可以省略，可以有一个或多个参数
+ * ([[Type] param1[, …]]) {
+ *    codeBlock;
+ *  };
+ */
+anonymousFunction() {
+  var list = ['one', 'two', 'three', 'four', 'five'];
+  list.forEach((element) {
+    print(element);
+  });
 }
